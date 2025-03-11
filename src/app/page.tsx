3295243f1324d2e1dfd5496df4e92bd9e8c4c966
koyu <me@koyu.space>
2025-03-11 22:43:17 +0100

    Simplify Home component by removing unused Image and redundant markup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,9 @@
 "use client";
 
 import CheckAuth from '@/utils/checkauth';
-import Image from 'next/image';
 
 export default function Home() {
   return (
-    <>
-      <CheckAuth />
-      <div className="container mx-auto">
-        <h1 className="text-4xl font-bold text-center">Hello, world!</h1>
-        <Image src="/img/pb-icon.svg" alt="Logo" width={300} height={300} />
-      </div>
-    </>
+    <CheckAuth />
   );
 }
